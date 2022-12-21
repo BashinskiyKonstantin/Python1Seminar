96623c0e65f38678c0766d60adf69a4487718225
Konstantin <>
2022-12-21 21:45:46 +0300

    task3

--- dz 1 seminar/task3.py
+++ dz 1 seminar/task3.py
@@ -7,3 +7,27 @@
 # - x=34; y=-30 -> 4
 # - x=2; y=4-> 1
 # - x=-34; y=-30 -> 3
+
+def inputCoordinates(n):
+    xy = ["X", "Y"]
+    list = []
+    for i in range(n):
+        list.append(int(input(f"Введите значение {xy[i]}: ")))
+    return list
+
+
+def checkCoordinates(xy):
+    if xy[0] != 0 and xy[1] != 0:
+        quarter = "четвертой"
+        if xy[0] > 0 and xy[1] > 0:
+            quarter = "первой"
+        elif xy[0] < 0 and xy[1] > 0:
+            quarter = "второй"
+        elif xy[0] < 0 and xy[1] < 0:
+            quarter = "третьей"
+        print(f"Точка ({xy[0]}, {xy[1]}) находится в {quarter} четверти плоскости")
+    else:
+        print(f"Значение координат X:({xy[0]}) и Y:({xy[1]}) не должны равняться нулю!")
+
+
+checkCoordinates(inputCoordinates(2))
