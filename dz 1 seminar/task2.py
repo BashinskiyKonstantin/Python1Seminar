85245a3b51b81d1dcda92cf1678a69c4d3a38038
Konstantin <>
2022-12-21 20:53:47 +0300

    task2

--- dz 1 seminar/task2.py
+++ dz 1 seminar/task2.py
@@ -1,2 +1,22 @@
 # Напишите программу для. проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def inputCoordinates(n):
+    xyz = ["X", "Y", "Z"]
+    list = []
+    for i in range(n):
+        list.append(input(f"Введите значение {xyz[i]}: "))
+    return list
+
+
+def checkPredicate(list):
+    oneCheking = not (list[0] or list[1] or list[2])
+    secondCheking = not list[0] and not list[1] and not list[2]
+    result = oneCheking == secondCheking
+    return result
+
+
+if checkPredicate(inputCoordinates(3)):
+    print("Утверждение истинно!")
+else:
+    print("Утверждение ложно!")
