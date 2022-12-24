782c49125415e657e01a65ed7e28fe8ba696397e
Konstantin <>
2022-12-25 00:22:13 +0300

    task5

--- dz 1 seminar/task5.py
+++ dz 1 seminar/task5.py
@@ -5,3 +5,17 @@
 
 # - A (3,6); B (2,1) -> 5,09
 # - A (7,-5); B (1,-1) -> 7,21
+
+def inputCoordinates(n):
+    xy1xy2 = ["X1", "Y1", "X2", "Y2"]
+    list = []
+    for i in range(n):
+        list.append(int(input(f"Введите координаты точки {xy1xy2[i]}: ")))
+    return list
+
+def CoordinateDistance(list):
+    result = ((list[2] - list[0]) ** 2 + (list[3] - list[1]) ** 2) ** (0.5)
+    return result
+
+
+print(f"Расстояние между точками: {format(CoordinateDistance(inputCoordinates(4)), '.2f')}")
