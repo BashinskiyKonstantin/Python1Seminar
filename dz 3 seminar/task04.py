33c4de2d56e9b708e75c5018daaec92d54f02cc0
Konstantin <>
2022-12-26 20:19:20 +0300

    dz3task04

--- dz 3 seminar/task04.py
+++ dz 3 seminar/task04.py
@@ -5,3 +5,12 @@
 # - 45 -> 101101
 # - 3 -> 11
 # - 2 -> 10
+
+num = int(input("Введите десятичное число: "))
+result = 0
+count = 1
+while num > 0:
+    result = int(result + num % 2 * count)
+    num = int(num/2)
+    count = count*10
+print(f'Двоичный вид числа -> {result}')
